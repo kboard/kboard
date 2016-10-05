121eef91335ef5cd53d06cedc07a2beb24ee2808
전현준 <guswnsxodlf@gmail.com>
2016-10-05 13:21:35 +0900

    Add url item

--- kboard/board/urls.py
+++ kboard/board/urls.py
@@ -6,5 +6,6 @@ from . import views
 
 app_name = 'board'
 urlpatterns = [
-    url(r'^$', views.new_post, name='new_post')
+    url(r'^$', views.new_post, name='new_post'),
+    url(r'^board/', views.post_list, name='post_list')
 ]
