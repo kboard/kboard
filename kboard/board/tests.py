d50b5696cd034dae35d6932624f3708a99ad02d1
전현준 <guswnsxodlf@gmail.com>
2016-10-08 19:29:51 +0900

    Modify uniitest

--- kboard/board/tests.py
+++ kboard/board/tests.py
@@ -31,6 +31,7 @@ class CreatePostPageTest(TestCase):
     def test_post_list_can_save_a_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
+        request.POST['post_board_id'] = Board.objects.first().id
         request.POST['post_title_text'] = 'NEW POST TITLE'
         request.POST['post_content_text'] = 'NEW POST CONTENT'
 
@@ -43,6 +44,7 @@ class CreatePostPageTest(TestCase):
     def test_new_post_page_redirects_after_POST(self):
         request = HttpRequest()
         request.method = 'POST'
+        request.POST['post_board_id'] = Board.objects.first().id
         request.POST['post_title_text'] = 'NEW POST TITLE'
         request.POST['post_content_text'] = 'NEW POST CONTENT'
 
@@ -57,8 +59,8 @@ class CreatePostPageTest(TestCase):
         self.assertEqual(Post.objects.count(), 0)
 
     def test_post_list_page_displays_all_list_titles(self):
-        Post.objects.create(title='turtle1', content='slow')
-        Post.objects.create(title='turtle2', content='slowslow')
+        Post.objects.create(board=Board.objects.first().id, title='turtle1', content='slow')
+        Post.objects.create(board=Board.objects.first().id, title='turtle2', content='slowslow')
 
         request = HttpRequest()
         response = post_list(request)
@@ -70,21 +72,37 @@ class CreatePostPageTest(TestCase):
 class PostViewTest(TestCase):
 
     def test_uses_list_template(self):
-        post_ = Post.objects.create(title='post of title', content='post of content')
+        post_ = Post.objects.create(board=Board.objects.first().id, title='post of title', content='post of content')
         response = self.client.get('/posts/%d/' % (post_.id,))
         self.assertTemplateUsed(response, 'view_post.html')
 
     def test_passes_correct_post_to_template(self):
-        other_post = Post.objects.create(title='other post of title', content='other post of content')
-        correct_post = Post.objects.create(title='correct post of title', content='correct post of content')
+        other_post = Post.objects.create(
+            board=Board.objects.first().id,
+            title='other post of title',
+            content='other post of content'
+        )
+        correct_post = Post.objects.create(
+            board=Board.objects.first().id,
+            title='correct post of title',
+            content='correct post of content'
+        )
 
         response = self.client.get('/posts/%d/' % (correct_post.id,))
 
         self.assertEqual(response.context['post'], correct_post)
 
     def test_view_post_page_displays_correct_title_and_content(self):
-        other_post = Post.objects.create(title='other post of title', content='other post of content')
-        correct_post = Post.objects.create(title='correct post of title', content='correct post of content')
+        other_post = Post.objects.create(
+            board=Board.objects.first().id,
+            title='other post of title',
+            content='other post of content'
+        )
+        correct_post = Post.objects.create(
+            board=Board.objects.first().id,
+            title='correct post of title',
+            content='correct post of content'
+        )
 
         response = self.client.get('/posts/%d/' % (correct_post.id,))
 
@@ -98,11 +116,13 @@ class PostModelTest(TestCase):
 
     def test_saving_and_retrieving_post(self):
         first_post = Post()
+        first_post.board = Board.objects.first().id
         first_post.title = 'first post of title'
         first_post.content = 'first post of content'
         first_post.save()
 
         second_post = Post()
+        second_post.board = Board.Obejcts.first().id
         second_post.title = 'second post of title'
         second_post.content = 'second post of content'
         second_post.save()
