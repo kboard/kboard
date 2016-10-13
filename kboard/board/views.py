7f4495b54608e8cbb38e2610fce3700425d1067b
hyesun03 <chsun0303@gmail.com>
2016-10-13 13:29:43 +0900

    Change textarea to summernote

--- kboard/board/views.py
+++ kboard/board/views.py
@@ -2,17 +2,19 @@ from django.shortcuts import render, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from board.models import Post, Board, Comment
+from board.forms import PostForm
 
 
 def new_post(request):
     board = Board.objects.get(id=request.GET['board'])
-    return render(request, 'new_post.html', {'board': board})
+    form = PostForm()
+    return render(request, 'new_post.html', {'board': board, 'form': form})
 
 
 def post_list(request, board_id):
     if request.method == 'POST':
         board = Board.objects.get(id=board_id)
-        Post.objects.create(board=board, title=request.POST['post_title_text'], content=request.POST['post_content_text'])
+        Post.objects.create(board=board, title=request.POST['post_title_text'], content=request.POST.get('fields', ''))
         return redirect('/board/'+str(board_id))
 
     posts_all_list = Post.objects.all()
--- kboard/kboard/settings.py
+++ kboard/kboard/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'board',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'kboard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,  'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+
+MEDIA_URL='/media/'
+
+
+# Summernote
+
+SUMMERNOTE_CONFIG = {}
