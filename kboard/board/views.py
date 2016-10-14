4de027d99fcddeba7101bcbd302bb436e4a2ff9c
전현준 <guswnsxodlf@gmail.com>
2016-10-14 18:33:55 +0900

    Modify views refer to urls.py

--- kboard/board/views.py
+++ kboard/board/views.py
@@ -5,18 +5,18 @@ from board.models import Post, Board, Comment
 from board.forms import PostForm
 
 
-def new_post(request):
-    board = Board.objects.get(id=request.GET['board'])
+def new_post(request, board_slug):
+    if request.method == 'POST':
+        board = Board.objects.get(slug=board_slug)
+        Post.objects.create(board=board, title=request.POST['post_title_text'], content=request.POST.get('fields', ''))
+        return redirect('/{}/'.format(board_slug))
+
+    board = Board.objects.get(slug=board_slug)
     form = PostForm()
     return render(request, 'new_post.html', {'board': board, 'form': form})
 
 
-def post_list(request, board_id):
-    if request.method == 'POST':
-        board = Board.objects.get(id=board_id)
-        Post.objects.create(board=board, title=request.POST['post_title_text'], content=request.POST.get('fields', ''))
-        return redirect('/board/'+str(board_id))
-
+def post_list(request, board_slug):
     posts_all_list = Post.objects.all()
     paginator = Paginator(posts_all_list, 10)  # Show 10 contacts per page
 
@@ -30,27 +30,27 @@ def post_list(request, board_id):
         # If page is out of range (e.g. 9999), deliver last page of results.
         posts = paginator.page(paginator.num_pages)
 
-    return render(request, 'post_list.html', {'posts': posts, 'board_id': board_id})
+    return render(request, 'post_list.html', {'posts': posts, 'board_slug': board_slug})
 
 
-def view_post(request, post_id):
+def view_post(request, board_slug, post_id):
     post = Post.objects.get(id=post_id)
 
-    return render(request, 'view_post.html', {'post': post, 'board_id': post.board.id})
+    return render(request, 'view_post.html', {'post': post, 'board_slug': board_slug})
 
 
 def board_list(request):
     board_count = Board.objects.all().count()
     if board_count == 0:
-        Board.objects.create(name='Default')
+        Board.objects.create(name='Default', slug='default')
 
     boards = Board.objects.all()
 
     return render(request, 'board_list.html', {'boards': boards})
 
 
-def new_comment(request):
+def new_comment(request, board_slug, post_id):
     if request.method == 'POST':
-        post = Post.objects.get(id=request.POST['post_id'])
+        post = Post.objects.get(id=post_id)
         Comment.objects.create(post=post, content=request.POST['comment_content'])
         return redirect(post)
