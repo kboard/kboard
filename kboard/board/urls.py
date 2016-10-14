4f8e4029a25a2f4011cb220cf7d784751556c3ed
전현준 <guswnsxodlf@gmail.com>
2016-10-14 18:32:20 +0900

    Refactor URLs refer to issue#34

--- kboard/board/urls.py
+++ kboard/board/urls.py
@@ -6,9 +6,9 @@ from . import views
 
 app_name = 'board'
 urlpatterns = [
-    url(r'^posts/new/$', views.new_post, name='new_post'),
-    url(r'^posts/(\d+)/$', views.view_post, name='view_post'),
-    url(r'^comment/new/$', views.new_comment, name='new_comment'),
-    url(r'^board/(\d+)/$', views.post_list, name='post_list'),
-    url(r'^$', views.board_list, name='board_list')
+    url(r'^$', views.board_list, name='board_list'),
+    url(r'^(?P<board_slug>[-\w]+)/$', views.post_list, name='post_list'),
+    url(r'^(?P<board_slug>[-\w]+)/new/$', views.new_post, name='new_post'),
+    url(r'^(?P<board_slug>[-\w]+)/(?P<post_id>\d+)/$', views.view_post, name='view_post'),
+    url(r'^(?P<board_slug>[-\w]+)/(?P<post_id>\d+)/new/$', views.new_comment, name='new_comment'),
 ]
