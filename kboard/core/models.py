e7517824e69557805a0995170aa8c7e5df41eac8
전현준 <guswnsxodlf@gmail.com>
2016-10-25 23:01:18 +0900

    Add Post model manager, Post queryset

--- kboard/core/models.py
+++ kboard/core/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 
 
+class PostQuerySet(models.QuerySet):
+    def search(self, query):
+        return self.filter(title__contains=query)
+
+
+class PostManager(models.Manager):
+    def get_queryset(self):
+        return PostQuerySet(self.model, using=self._db)
+
+    def search(self, query):
+        return self.get_queryset().search(query)
+
+
 class TimeStampedModel(models.Model):
     create_time = models.DateTimeField(auto_now_add=True)
 
