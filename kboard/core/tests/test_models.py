cd0ffbc4bcbc95e927e5d89df4e7a0eaeb0f48a8
cjh5414 <cjh5414@gmail.com>
2016-10-30 19:35:50 +0900

    Add unittest for modified_time im TimeStampedModel

--- kboard/core/tests/test_models.py
+++ kboard/core/tests/test_models.py
@@ -2,12 +2,15 @@ from datetime import datetime
 from datetime import timezone
 from datetime import timedelta
 
+from django.db import models
 from django.test import TestCase
 
 from core.models import TimeStampedModel
 
 
 class TestModelInheritedTimeStampedModel(TimeStampedModel):
+    name = models.TextField(default='')
+
     class Meta:
         app_label = 'core'
 
@@ -20,3 +23,14 @@ class TestTimeStampedModel(TestCase):
         time_after_create = datetime.now(timezone.utc)
 
         self.assertGreater(timedelta(minutes=1), time_after_create - test_model.created_time)
+
+    def test_modified_time_after_modify_Inherited_TimeStampedModel(self):
+        test_model = TestModelInheritedTimeStampedModel()
+        test_model.save()
+
+        test_model.name = "modified name"
+        test_model.save()
+
+        time_after_modify = datetime.now(timezone.utc)
+
+        self.assertGreater(timedelta(minutes=1), time_after_modify - test_model.modified_time)
