d4148816c6dc197d82502464cbca673e14d27b27
cjh5414 <cjh5414@gmail.com>
2016-10-30 19:36:52 +0900

    Add modified_time on TimeStampedModel

--- kboard/core/models.py
+++ kboard/core/models.py
@@ -28,6 +28,7 @@ class PostManager(models.Manager):
 
 class TimeStampedModel(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
