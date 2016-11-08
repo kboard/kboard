2923fcaf476bdbde3c428a5a0a1ec988170274f7
hyesun03 <chsun0303@gmail.com>
2016-11-09 08:33:54 +0900

    Refactoring registration form test

--- kboard/functional_test/test_registration_form.py
+++ kboard/functional_test/test_registration_form.py
@@ -7,18 +7,12 @@ class RegistrationFormTest(FunctionalTest):
         self.browser.get(self.live_server_url + '/accounts/register/')
 
         # 가입에 필요한 정보를 작성한다.
-        usernamebox = self.browser.find_element_by_id("id_username")
-        usernamebox.send_keys("chickenlover01")
-
-        emailbox = self.browser.find_element_by_id("id_email")
-        emailbox.send_keys("chsun0303@naver.com")
-
-        password1box = self.browser.find_element_by_id("id_password1")
-        password1box.send_keys("abcd0000")
+        self.register_send_key("id_username", "chickenlover01")
+        self.register_send_key("id_email", "chsun0303@naver.com")
+        self.register_send_key("id_password1", "abcd0000")
 
         # 손이 미끄러져서 똑같은 비밀번호를 치지못했다.
-        password2box = self.browser.find_element_by_id("id_password2")
-        password2box.send_keys("abcd0009")
+        self.register_send_key("id_password1", "abcd0009")
 
         checkbox = self.browser.find_element_by_id("agree")
         checkbox.click()
@@ -34,18 +28,12 @@ class RegistrationFormTest(FunctionalTest):
         self.browser.get(self.live_server_url + '/accounts/register/')
 
         # 가입에 필요한 정보를 작성한다.
-        usernamebox = self.browser.find_element_by_id("id_username")
-        usernamebox.send_keys("chickenlover01")
+        self.register_send_key("id_username", "chickenlover01")
+        self.register_send_key("id_email", "chsun0303@naver.com")
 
-        emailbox = self.browser.find_element_by_id("id_email")
-        emailbox.send_keys("chsun0303@naver.com")
-
-        # 비밀번호는 최소 8자로 만들어야 하지만 아무 생각이 없다.
-        password1box = self.browser.find_element_by_id("id_password1")
-        password1box.send_keys("abcd000")
-
-        password2box = self.browser.find_element_by_id("id_password2")
-        password2box.send_keys("abcd000")
+        # 귀찮아서 비밀번호를 짧게 만든다.
+        self.register_send_key("id_password1", "abcd")
+        self.register_send_key("id_password2", "abcd")
 
         checkbox = self.browser.find_element_by_id("agree")
         checkbox.click()
