15afd58e1644bd2fc9ba9b480a973e2acc3fbd59
hyesun03 <chsun0303@gmail.com>
2016-11-09 14:29:27 +0900

    Add functional test for password and username are the same

--- kboard/functional_test/test_registration_form.py
+++ kboard/functional_test/test_registration_form.py
@@ -65,4 +65,23 @@ class RegistrationFormTest(FunctionalTest):
         error = self.browser.find_element_by_class_name("errorlist")
         self.assertTrue("This password is entirely numeric.", error)
 
-    ## 어디까지 테스트를 작성해야 할지 모르겠다..
+    def test_password_same_as_username(self):
+        # 혜선이는 회원가입을 하고싶어한다.
+        self.browser.get(self.live_server_url + '/accounts/register/')
+
+        # 가입에 필요한 정보를 작성한다.
+        self.register_send_key("id_username", "chickenlover01")
+        self.register_send_key("id_email", "chsun0303@naver.com")
+
+        # 귀찮아서 비밀번호를 username과 똑같이 만든다.
+        self.register_send_key("id_password1", "chickenlover01")
+        self.register_send_key("id_password2", "chickenlover01")
+
+        checkbox = self.browser.find_element_by_id("agree")
+        checkbox.click()
+
+        self.click_submit_button()
+
+        # Password 아래 쪽에 username와 비밀번호는 유사하면 안된다는 에러메시지가 나온다.
+        error = self.browser.find_element_by_class_name("errorlist")
+        self.assertTrue("A user with that username already exists.", error)
