b34ebca5ea046c5451f98a508b1743d234bfb942
hyesun03 <chsun0303@gmail.com>
2016-11-12 03:16:01 +0900

    Change to TimeStampedModel

--- kboard/accounts/models.py
+++ kboard/accounts/models.py
@@ -1,17 +1,15 @@
 from django.db import models
 from django.conf import settings
 
+from core.models import TimeStampedModel
 
-class Account(models.Model):
+
+class Account(TimeStampedModel):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
     )
     fullName = models.CharField(max_length=150)
     status = models.CharField(max_length=20)
-
-    # Automatic timestamps
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.fullName
