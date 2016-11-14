5ff15729ddc514222f8d94711b6a0c1888744a04
hyesun03 <chsun0303@gmail.com>
2016-11-15 07:52:09 +0900

    Add password reset/change tempaltes

--- kboard/accounts/urls.py
+++ kboard/accounts/urls.py
@@ -1,8 +1,7 @@
-from django.conf.urls import url, include
-from accounts.views import RegistrationView
-from accounts.forms import RegistrationForm
+from django.conf.urls import url
 
 from . import views
+
 
 app_name = 'accounts'
 urlpatterns = [
