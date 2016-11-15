484454a0b7e3f6037718ae4fcd05b4a4377235a0
전현준 <guswnsxodlf@gmail.com>
2016-11-15 19:34:32 +0900

    Modify FT

--- kboard/functional_test/test_post_validation.py
+++ kboard/functional_test/test_post_validation.py
@@ -17,17 +17,12 @@ class PostValidationTest(FunctionalTest):
         self.browser.switch_to.default_content()
 
         # title의 입력 상자가 비어있는 상태에서 '등록' 버튼을 누른다.
-        self.click_submit_button()
+        submit_button = self.browser.find_element_by_class_name('post-submit-button')
+        submit_button.click()
 
         # 제목과 내용을 입력해달라는 경고메시지가 떠서 확인을 누른다.
         alert = self.browser.switch_to_alert()
         alert.accept()
-
-        # 제목을 입력해서 다시 시도하면 정상 처리된다.
-        titlebox = self.browser.find_element_by_id('id_post_title')
-        titlebox.send_keys('Title of This Post')
-        self.click_submit_button()
-        self.check_for_row_in_list_table('id_post_list_table', 'Title of This Post')
 
     def test_cannot_add_empty_content(self):
         self.browser.get(self.live_server_url)
@@ -42,16 +37,9 @@ class PostValidationTest(FunctionalTest):
         titlebox.send_keys('Title of This Post')
 
         # content 입력 상자가 비어있는 상태에서 '등록' 버튼을 누른다.
-        self.click_submit_button()
+        submit_button = self.browser.find_element_by_class_name('post-submit-button')
+        submit_button.click()
 
         # 제목과 내용을 입력해달라는 경고메시지가 떠서 확인을 누른다.
         alert = self.browser.switch_to_alert()
         alert.accept()
-
-        # 내용을 입력해서 다시 시도하면 정상 처리된다.
-        contentbox = self.get_contentbox()
-        contentbox.send_keys('Content of This Post')
-        self.browser.switch_to.default_content()
-        time.sleep(1)
-        self.click_submit_button()
-        self.check_for_row_in_list_table('id_post_list_table', 'Title of This Post')
