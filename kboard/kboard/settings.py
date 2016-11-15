2a6b44512166616d9b94991984c219f8bf6924e9
전현준 <guswnsxodlf@gmail.com>
2016-11-15 18:19:47 +0900

    Modify settings to use mysql if debug is false

--- kboard/kboard/settings.py
+++ kboard/kboard/settings.py
@@ -87,13 +87,22 @@ WSGI_APPLICATION = 'kboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'kboard',
+            'USER': 'root',
+            'PASSWORD': 'root'
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
