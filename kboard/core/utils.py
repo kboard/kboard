1ccdb076d6f07e14ca40c27703a61f155ad60bfa
전현준 <guswnsxodlf@gmail.com>
2016-11-19 21:32:21 +0900

    Add get_ip function in util

--- kboard/core/utils.py
+++ kboard/core/utils.py
@@ -29,3 +29,12 @@ def get_pages_nav_info(page, nav_chunk_size=10):
         'current_page_num': page.number,
         'next_nav_page': next_nav_page}
     return pages_nav_info
+
+
+def get_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip_address = x_forwarded_for.split(',')[-1].strip()
+    else:
+        ip_address = request.META.get('REMOTE_ADDR')
+    return ip_address
